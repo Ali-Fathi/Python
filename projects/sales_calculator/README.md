c09c1e94271ba4df854a1785c91db7c9dbebd571
ali-fathi <fathialo81@gmail.com>
2022-02-13 01:55:40 -0700

    Added Python Sales Tax Calculator projects and renamed some projects files

---
+++ projects/sales_calculator/README.md
@@ -0,0 +1,9 @@
+# Sales Tax Calculator in Python.
+
+## What is Sales Tax ?
+
+** A sales tax is a consumption tax paid to a government on the sale of certain goods and services **
+
+## How it works
+
+---
---
+++ projects/sales_calculator/sales_calculator.py
@@ -0,0 +1,8 @@
+# Print greeting
+print("Welcome to Sales Calculator!.")
+# ask for the bill
+before_tax_price = float(input("What is your your bill $"))
+# ask for sales percent
+sales_tax_rate = float(input("What is your sales percent? 5, 10, 15 ?\n"))
+
+# C
