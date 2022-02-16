91ed2b87a15e6f9e0a9ea03c8ccc5592186005db
ali-fathi <fathialo81@gmail.com>
2022-02-16 11:00:06 -0700

    Added the logic part of rock, paper and scissors project

--- projects/rock_paper_scissors/main.py
+++ projects/rock_paper_scissors/main.py
@@ -1,6 +1,3 @@
-import imp
-
-
 import random
 
 rock = '''
@@ -35,15 +32,28 @@ choices = [rock, paper, scissors]
 # program greetings
 print("Welcome to the")
 # display choice for the user
-user_choice = int(input("What do you choose? type 0 for the Rock, 1 for the Paper or 2 for Scissors. "))
+user_choice = int(input("What do you choose? type 0 for the Rock, 1 for the Paper or 2 for Scissors.\n"))
 print(f"You chose: {choices[user_choice]}")
 
 # computer
-computer_chose = random.choice(choices)
-print(f"Computer choice: {computer_chose}")
+computer_choice = random.randint(0, 2)
+print(f"Computer chose: {computer_choice}")
 
 # Logic 
 # The game rules:
 #     - Rock wins against scissors.
 #     - Scissors win against paper.
 #     - Paper wins against rock
+
+if user_choice >= 3 or user_choice < 0:
+    print("You typed an invalid number!")
+elif user_choice == 0 and computer_choice == 2:
+    print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+    print("You lose")
+elif computer_choice > user_choice:
+    print("You lose")
+elif computer_choice == user_choice:
+    print("Its a draw")
+else:
+    print("You typed an invalid number!")
