dbe7994ff09fa30957c7fa62ef3e4757aad13c81
ali-fathi <fathialo81@gmail.com>
2022-06-15 20:26:03 -0600

    add game art and checked for correct choice of options

--- lessons/lesson-04/lesson-project/main.py
+++ lessons/lesson-04/lesson-project/main.py
@@ -1,5 +1,6 @@
 # TODO 1: import random module
 import random
+from secrets import choice
 
 rock = '''
     _______
@@ -29,8 +30,21 @@ scissors = '''
 '''
 
 art = [rock, paper, scissors]
-
+art_len = len(art)
 # TODO 2: Generator random from the list
-user_choice = random.choice(art)
+user_choice = int(input("What do you choice? Type 0 for Rock, 1 for Paper, and 2 for Scissor.\n"))
+computer_choice = random.choice(art)
+user_choice_of_art = art[user_choice]
+# TODO 3: Output what user and computer chose to test
+print(f"You chose:\n {user_choice_of_art}")
+print(f"Computer chose:\n {computer_choice}")
 
-# print(f"you choice {choice_of_art} {user_choice}")
+# Game 
+
+if computer_choice == user_choice_of_art:
+    print(f"You chose:\n {user_choice_of_art}")
+    print(f"Computer chose:\n {computer_choice}")
+    print(f"\n\n Its a draw!")
+
+    
+
